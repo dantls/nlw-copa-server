44700a035b462bc17c5f9933cb24ecfc0ce08755
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2022-11-13 23:11:21 -0400

    Add counts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import {z} from 'zod';
+import ShortUniqueId from 'short-unique-id';
 
 
 import { PrismaClient } from '@prisma/client';
@@ -23,6 +25,37 @@ async function bootstrap(){
     const pools = await prisma.pool.count();
 
     return{ count: pools}
+  });
+  fastify.get('/user/count', async () => {
+    const pools = await prisma.user.count();
+
+    return{ count: pools}
+  });
+  fastify.get('/guesses/count', async () => {
+    const pools = await prisma.guess.count();
+
+    return{ count: pools}
+  });
+
+  fastify.post('/pools', async (request, reply) => {
+
+    const createPoolBody = z.object({
+      title: z.string()
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6});
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data:{
+        title,
+        code
+      }
+    })
+
+    return reply.status(201).send({ code })
   });
 
   await fastify.listen({
